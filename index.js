05fc3fc07ab923eb23649a81bd3e29187ee47ce3
Ron Martin <ronmartin89@gmail.com>
2020-01-15 13:02:20 -0800

    Functional stubs and user stories

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 const apiKey = 'AIzaSyDJUlKU5COjmdJ-hD2yBR3XKGtWjxqjtkw';
+
+// User must be able to search for parks in one or more states
+// User must be able to set MAX NUMBER OF RESULTS with a default of 10
+// the search must trigger a call to NPS's API
+// The parks in the given state must be displayed on the page
+//          Include at least:
+//           full name
+//           description
+//           website URL
+//           OPTIONAL: parks address
+// We need to learn results ie .empty() or .val('')
+
+// INPUT WITH SUBMIT, . Required input
+// INPUT with MAX RESULTs
+// handle submit
+
+
+/** fetch the parks with our API key */
+function getParks() {};
+
+/** Extract  data from responseJson and make it into usable string for inserting into HTML */
+function extractData() {};
+
+/** create HTML template */
+
+function createHTML() {};
+
+/** event listener */
+function handleSubmit() {};
+
+function main() {
+    handleSubmit();
+};
+
+$(main);
