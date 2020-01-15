d660edd57362de4cbfd3dc574b0718f12751bb87
gage117 <57643501+gage117@users.noreply.github.com>
2020-01-15 16:18:13 -0600

    added more functionality

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const endpoint =  'https://developer.nps.gov/api/v1/alerts';
-const apiKey = 'AIzaSyDJUlKU5COjmdJ-hD2yBR3XKGtWjxqjtkw';
+const searchURL =  'https://developer.nps.gov/api/v1/parks';
+const apiKey = 'Qk2tjWXECgHdoIMbemJObeaaMAWYbyXyjA1Xane9';
 
 // User must be able to search for parks in one or more states
 // User must be able to set MAX NUMBER OF RESULTS with a default of 10
@@ -19,28 +19,73 @@ const apiKey = 'AIzaSyDJUlKU5COjmdJ-hD2yBR3XKGtWjxqjtkw';
 
 /** fetch the parks with our API key */
 function getParks(query, resultsNum) {
-    console.log(query);
-};
+  const addresses = 'addresses';
+  const params = {
+    api_key: apiKey,
+    stateCode: query,
+    limit: resultsNum,
+    fields: addresses
+  };
+  const queryString = formatQueryParams(params);
+  const url = searchURL + '?' + queryString;
+
+  console.log(url);
+
+  fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => createHTML(responseJson))
+    .catch(err => {
+      $('#js-error-message').text(`Something went wrong: ${err.message}`);
+    });
+}
+
+function formatQueryParams(params) {
+    const queryItems = Object.keys(params)
+      .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+    return queryItems.join('&');
+}
 
 /** Extract  data from responseJson and make it into usable string for inserting into HTML */
 function extractData() {};
 
 /** create HTML template */
 
-function createHTML() {};
+function createHTML(responseJson) {
+  $('#results-list').empty();
+  // iterate through the data array
+  for (let i = 0; i < responseJson.data.length; i++){
+    // for each video object in the data 
+    //array, add a list item to the results 
+    //list with the video title, description,
+    //and thumbnail
+    $('#results-list').append(
+      `<li><h3>${responseJson.data[i].fullName}</h3>
+      <p>${responseJson.data[i].description}</p>
+      <p>${responseJson.data[i].addresses}</p>
+      <p><a href='${responseJson.data[i].url}>Click for More Info</a></p>
+      </li>`
+    )};
+  //display the results section  
+  $('#results').removeClass('hidden');
+};
 
 /** event listener */
 function handleSubmit() {
-    $('form').submit( event => {
-        event.preventDefault();
-        const searchTerm = $('#state-input').val();
-        const maxResults = $('#max-results-input').val();
-        getParks(searchTerm, maxResults);
-    });
+  $('form').submit( event => {
+    event.preventDefault();
+    const searchTerm = $('#state-input').val();
+    const maxResults = $('#max-results-input').val();
+    getParks(searchTerm, maxResults);
+  });
 };
 
 function main() {
-    handleSubmit();
+  handleSubmit();
 };
 
 $(main);
